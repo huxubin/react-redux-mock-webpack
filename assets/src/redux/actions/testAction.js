1a8a390eea77725a5b07ebf1a6d4d81b2511e7b0
huxubin <w_huxubin@126.com>
2016-08-25 14:10:48 +0800

    Update testAction.js

--- assets/src/redux/actions/testAction.js
+++ assets/src/redux/actions/testAction.js
@@ -1,1 +1,25 @@
+/**
+ * 文件说明: 测试Actions
+ * 详细描述:
+ * 创建者:   hxb
+ * 创建时间: 2016/8/25
+ * 变更记录:
+ */
+import * as Types from '../utils/actionTypes.js';
+import * as testService from '../../../../service/testService.js';
 
+// test
+export function testAction(data) {
+    return (dispatch) => {
+        testService.getTestData(data).then((res)=>{
+            dispatch({
+                type: Types.GET_TEST_DATA,
+                store: res.data
+            });
+        },(err)=> {
+            dispatch({
+                type: Types.ERROR_MESSAGE
+            });
+        })
+    }
+}
