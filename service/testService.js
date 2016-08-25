bad30be2f66a8928193694f1eece4653946298f6
huxubin <w_huxubin@126.com>
2016-08-25 14:21:50 +0800

    Create testService.js

---
+++ service/testService.js
@@ -0,0 +1,23 @@
+/**
+ * 文件说明: 测试service
+ * 详细描述:
+ * 创建者: hxb
+ * 创建时间: 2016/8/25
+ * 变更记录:
+ */
+import Q from 'q';
+import HttpClient from '../assets/src/redux/utils/httpClient';
+
+/*
+ * test
+ * */
+export function getTestData(data) {
+    let defer = Q.defer();
+    HttpClient.post('/contact/list',data)
+        .then((res) => {
+            defer.resolve(res);
+        }, (err)=> {
+            defer.reject(err);
+        });
+    return defer.promise;
+}
